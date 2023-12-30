83c811d185a2b5b6bc8c07690a9d02d5ab59093e
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-12-30 15:15:24 +0530

    sidebar don

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import AdminSidebar from '../components/AdminSidebar';
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='admin-container'>
+      <AdminSidebar/>
+      <main>hghg</main>
+    </div>
   )
 }
 
