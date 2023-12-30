9ef5975f1d198c835aaed6e6fe6138fa7f7f3a61
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-12-30 10:50:07 +0530

    mixing css don

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Ensure correct path and component name
-import {lazy} from 'react';
-const Dashboard = lazy(()=> import("./pages/dashboard"));
+import {lazy,Suspense} from 'react';
+import Loader from "./components/Loader"
+const Dashboard = lazy(()=> import("./pages/Dashboard"));
 const Customers = lazy(()=> import("./pages/Customers"));
 const Product = lazy(()=> import("./pages/Product"));
 const Transaction = lazy(()=> import("./pages/Transaction"));
@@ -9,13 +9,16 @@ const Transaction = lazy(()=> import("./pages/Transaction"));
 const App = () => { // Use '=' instead of '=' in function declaration
   return (
     <Router>
+      <Suspense fallback={<Loader/>}/>
       <Routes>
+        
         <Route path="/admin/Dashboard" element={<Dashboard />} /> {/* Ensure correct component name */}
         <Route path="/admin/Customers" element={<Customers />} />
         <Route path="/admin/Product" element={<Product />} />
         <Route path="/admin/Transaction" element={<Transaction />} />
       
       </Routes>
+      <Suspense/>
     </Router>
   );
 };
