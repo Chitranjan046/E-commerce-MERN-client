6d1db00151ff9cd339c7055ffc82021683d1d011
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2023-12-30 16:33:18 +0530

    Dashboard search bar don

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import AdminSidebar from '../components/AdminSidebar';
-const Dashboard = () => {
-  return (
-    <div className='admin-container'>
-      <AdminSidebar/>
-      <main>hghg</main>
-    </div>
-  )
-}
+import { FaRegBell } from "react-icons/fa";
+import AdminSidebar from "../components/AdminSidebar";
+import { BsSearch } from "react-icons/bs";
+import userImg from "../assets/userImg.jpg";
 
-export default Dashboard;
+
+const dashboard = () => {
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="dashboard">
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell />
+          <img src={userImg} alt="User" />
+        </div>
+
+      </main>
+    </div>
+  );
+};
+
+export default dashboard;
